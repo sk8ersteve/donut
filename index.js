3329a1cfcc22e7830cbf536754555d7d2d90cad1
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2022-07-03 14:44:23 -0700

    Cleanup undex.js

--- index.js
+++ index.js
@@ -24,33 +24,38 @@ async function run() {
     const buff = new Uint8ClampedArray(wasm.memory.buffer, screen.get_screen(), 4 * width * height);
     const imgData = new ImageData(buff, width);
 
+    ctx.putImageData(imgData, 0, 0);
+
     const renderLoop = () => {
-        ctx.putImageData(imgData, 0, 0);
+        // Cycle through color
+        if (stage == 0) {
+            r -= 5;
+            g += 5;
+            if (r == 0 || g == 255)
+                stage = 1;
+        } else if (stage == 1) {
+            g -= 5;
+            b += 5;
+            if (g == 0 || b == 255)
+                stage = 2;
+        } else if (stage == 2) {
+            b -= 5;
+            r += 5;
+            if (b == 0 || r == 255)
+                stage = 0;
+        }
+        screen.set_donut_color(r, g, b, 255);
+
+        // Spin donut
+        A += 0.05;
+        B += 0.05;
         if (A > 6.28)
             A = 0.0;
         if (B > 6.28)
             B = 0.0;
-        screen.set_donut_color(r, g, b, 255);
         screen.draw_donut(A, B);
-        A += 0.01;
-        B += 0.01;
-        if (stage == 0) {
-            r -= 1;
-            g += 1;
-            if (r == 0 || g == 255)
-                stage = 1;
-        } else if (stage == 1) {
-            g -= 1;
-            b += 1;
-            if (g == 0 || b == 255)
-                stage = 2;
-        } else if (stage == 2) {
-            b -= 1;
-            r += 1;
-            if (b == 0 || r == 255)
-                stage = 0;
-        }
-        // screen.draw_circle();
+
+        ctx.putImageData(imgData, 0, 0);
         requestAnimationFrame(renderLoop);
     }
 
